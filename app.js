873f5f3b9552be98df6abfa6f6a77020da39e12d
toukircse <toukirahmed610@gmail.com>
2022-09-03 23:49:04 +0600

    add catagories

--- app.js
+++ app.js
@@ -1,61 +1,49 @@
-const loadAllNews =async() =>{
-    const response = await fetch('https://openapi.programming-hero.com/api/news/categories');
-    const data = await response.json();
-    displayAllNews(data.data.news_category)
-    }
-const displayAllNews = allNews =>{
-    console.log(allNews);
-    const newsContainer = document.getElementById('news-container')
-    allNews.forEach(news => {
-        console.log(news.category_id)
-    const newsDiv = document.createElement('div')
-    newsDiv.classList.add('d-flex')
-    newsDiv.innerHTML = `
-    <ul><li><button class="btn" onclick="loadNewsCard(${news.category_id})" href="">${news.category_name}</button></li></ul>
-    `
-    newsContainer.appendChild(newsDiv);
+const loadNews = async () => {
+  const url = `https://openapi.programming-hero.com/api/news/categories`;
 
-    });
-    
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    displayCategories(data.data.news_category);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+// fetching all categories
+const displayCategories = categories => {
+  categories.forEach(category => {
+    insertData(category, 'category-container');
+  });
+};
+// inserting all categories on page
+const insertData = (data, id) => {
+  const dataElement = document.createElement('div');
 
-}
-const loadNewsCard = async(id)=>{
-    const response = await fetch(`https://openapi.programming-hero.com/api/news/category/${id}`);
-    const data = await response.json();
-    displayNewsCard(data.data)
-}
-const displayNewsCard = cards =>{
-    const newsCard = document.getElementById('news-card');
-    newsCard.innerHTML='';
-    cards.forEach(card =>{
-        console.log(card)
-    const cardDiv = document.createElement('div');
-    cardDiv.innerHTML=`
-    <div class="card mb-3">
-    <div class="row g-0">
-      <div class="col-md-4 p-3">
-        <img src="${card.image_url}" class="img-fluid rounded-start" alt="...">
-      </div>
-      <div class="col-md-8">
-        <div class="card-body">
-          <h5 class="card-title">${ card.title}</h5>
-          <p class="card-text py-3">${card.details.length > 300 ? card.details.slice(0,300) + '...' : card.details}</p>
-          <div class="d-flex justify-content-between">
-            <div class="d-flex"><img width="30" height="24" class ="img-fluid rounded-circle"src="${card.author.img}" alt="" srcset="">
-            <p class="ms-2 fs-5">${card.author.name}</p>
-            </div>
-            <div><i class="fa-solid fa-eye"> ${card.total_view} </i></div>
-            <div><button class="btn btn-primary">Details</button></div>
-            
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-    `
-    newsCard.appendChild(cardDiv);
-    })
-}
-loadNewsCard();
-loadAllNews();
+  dataElement.innerHTML = `
+    <button onclick="loadThisIdNews(${data.category_id})">${data.category_name}</button>
+  `;
+
+  const dataField = document.getElementById(id);
+
+  dataField.appendChild(dataElement);
+};
+
+// load category specific news
+const loadThisIdNews = async category_id => {
+  toggleSpinner(true);
+
+  const categoryId = '0' + category_id;
+
+  // const url = `https://openapi.programming-hero.com/api/news/category/`;
+  const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    displayNewsByCategory(data.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
